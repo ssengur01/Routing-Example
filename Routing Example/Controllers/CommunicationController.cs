e1d72ccebf14718e01577685e167037488737c9c
ssengur01 <sengursevval03@gmail.com>
2023-10-05 12:37:04 +0300

    Add project files.

---
+++ Routing Example/Controllers/CommunicationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Routing_Example.Controllers
+{
+    public class CommunicationController: Controller
+    {
+        public IActionResult İletişim()
+        {
+            return View();
+        }
+    }
+}
